4777c34bca10a1f84e0519d659f6e185b02eaf73
shahadbasalim <shahadbasalim@gmail.com>
2025-03-09 12:23:11 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  result = '';
+  getAction(item: any) {
+    this.result += item; // result = result + item
+  }
+
+  equal() {
+    try {
+      this.result = eval(this.result);
+    } catch (error) {
+      this.result = 'Error';
+    }
+  }
+
+  reset() {
+    this.result = '';
+  }
+}
