c4b596c66bb4509f6fa91cfe532fb2e0942e1d9b
shahadbasalim <shahadbasalim@gmail.com>
2025-03-10 10:17:09 +0300

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,27 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  result = '';
-  getAction(item: any) {
-    this.result += item; // result = result + item
-  }
+  // result = '';
+  // getAction(item: any) {
+  //   this.result += item; // result = result + item
+  // }
 
+  // equal() {
+  //   try {
+  //     this.result = eval(this.result);
+  //   } catch (error) {
+  //     this.result = 'Error';
+  //   }
+  // }
+
+  // reset() {
+  //   this.result = '';
+  // }
+
+  result: string = '';
+  getAction(item: any) {
+    this.result += item;
+  }
   equal() {
     try {
       this.result = eval(this.result);
